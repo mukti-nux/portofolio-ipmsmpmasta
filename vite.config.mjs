2ca88598bcdce4cc09ca20a24f16fa63ca82809b
root <root@DESKTOP-HsnMkt.localdomain>
2025-09-06 17:03:23 +0700

    oke rev

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,8 +5,10 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
   build: {
-    outDir: "dist", // ✅ standar Vite
+    outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
